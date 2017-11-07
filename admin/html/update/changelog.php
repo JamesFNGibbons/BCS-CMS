8ba679b06eeaddfac7e522c9fa4eef1ce66edf36
jgibbons <jgibbons@quemit.com>
2017-11-07 10:00:34 +0000

    Updated changelog.php

--- admin/html/update/changelog.php
+++ admin/html/update/changelog.php
@@ -1,8 +1,7 @@
 <h3 style='margin: 0; padding: 0;'>BCS Website Studio Version <?php print Settings::get('software_version'); ?></h3>
 <p>In this update we have added the following: </p>
 <ul>
-  <li>Plugins - You can now install plugins to improve the software.</li>
-  <li>Website Customizer - An easy way to change your website</li>
-  <li>Page Manager - Create and update the pages on your website.</li>
-  <li>Media Center - Upload and manage files</li>
+  <li>The navigation manager</li>
+  <li>The navigation frontend API</li>
+  <li>Direct support for the bootstrap navigation bar</li>
 </ul>
